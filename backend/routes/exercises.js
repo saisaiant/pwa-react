6dca26f0588c69ced5d833050320b2fd51cb7761
Sai Aung Naing Tun <saisaiant@gmail.com>
2020-06-07 12:15:52 +0630

    edit gitignore

---
+++ backend/routes/exercises.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+let Exercise = require("../models/exercise.model");
+
+router.route("/").get((res, req) => {
+  Exercise.find()
+    .then((exercise) => res.json(exercise))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((res, req) => {
+  const username = req.body.username;
+  const description = req.body.description;
+  const duration = Number(req.body.duration);
+  const date = Date.parse(req.body.date);
+
+  const newExercise = new Exercise({
+    username,
+    description,
+    duration,
+    date,
+  });
+
+  newExercise
+    .save()
+    .then(() => res.json("Exercise added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
